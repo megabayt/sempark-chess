dec20082352250e246d9a1602ced83c7c2da0cdd
Artemiy Martyanov <artemiy@martyanov.xyz>
2020-02-20 23:28:42 +0300

    add vuetify

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import { HttpLink } from 'apollo-link-http'
 import { onError } from 'apollo-link-error';
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import VueApollo from 'vue-apollo'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
 import get from 'lodash/get';
 
 import App from './App.vue'
@@ -13,8 +16,8 @@ import { GQL_AUTH_TOKEN } from './contants';
 import eventBus from './eventBus';
 
 const uri = process.env.NODE_ENV === 'production'
-  ? 'https://api.sempark.xyz/graphql'
-  : 'http://localhost:1337/graphql';
+? 'https://api.sempark.xyz/graphql'
+: 'http://localhost:1337/graphql';
 
 const httpLink = new HttpLink({ uri });
 
@@ -52,11 +55,15 @@ const apolloProvider = new VueApollo({
   defaultClient: apolloClient,
 })
 
+const vuetify = new Vuetify({})
+
 Vue.use(VueApollo)
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
 new Vue({
   apolloProvider,
+  vuetify,
   render: h => h(App),
 }).$mount('#app')
---
+++ vue.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  configureWebpack: {
+    rules: [
+      {
+        test: /\.s(c|a)ss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            // Requires sass-loader@^8.0.0
+            options: {
+              implementation: require('sass'),
+              sassOptions: {
+                fiber: require('fibers'),
+                indentedSyntax: true // optional
+              },
+            },
+          },
+        ],
+      },
+    ],
+  }
+}
